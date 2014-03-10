36ede14f25c6a7faac3907991083b7401ad98b1d
dylanholmes <dylanholmes206@gmail.com>
2014-03-10 13:27:05 -0700

    The plot for CaseII is looking way better now

--- matlab/caseII.m
+++ matlab/caseII.m
@@ -10,87 +10,72 @@ PsiPrime0 = 1;
 PsiDoublePrime0 = 0;
 
 % Parameters
-nohiring = [0; 0];
-betas = 1:0.5:2;
-gammas = fliplr(-2:0.5:0);
-hiring = zeros(2, length(betas)*length(gammas));
-hiringCount = 1;
-% for p = 1:length(betas)
-%   for q = 1:length(gammas)
-%     hiring(1, hiringCount) = betas(p);
-%     hiring(2, hiringCount) = gammas(q);
-%     hiringCount = hiringCount + 1;
-%   end
-% end
-
-for p = 1:length(gammas)
-  for q = 1:length(betas)
-    hiring(1, hiringCount) = betas(q);
-    hiring(2, hiringCount) = gammas(p);
-    hiringCount = hiringCount + 1;
-  end
-end
-
-parameters = [nohiring, hiring];
+betas = [0.5 1 2];
+gammas = [0 -0.5 -1 -1.25];
 
 % Curves
 tspan = t0:0.01:td; % 30 days
-curves = length(parameters);
+curves = length(betas) * length(gammas);
 
 % Figure f
 f = figure;
-fontsize = 14;
-title('Nondimensionalized Progress vs Time', 'FontSize', fontsize+5);
-ylabel('\Psi ',  'FontSize', fontsize+10, 'rot', 0);
-xlabel('T', 'FontSize', fontsize+10);
-set(gca, 'ColorOrder', hsv(curves), 'FontSize', fontsize);
-axis([t0 td -2 td], 'square');
-hold all;
+for p = 1:4
+  sp(p) = subplot(2,2,p);
+  fontsize = 14;
+  title('Nondimensionalized Progress vs Time', 'FontSize', fontsize+5);
+  ylabel('\Psi ',  'FontSize', fontsize+10, 'rot', 0);
+  xlabel('T', 'FontSize', fontsize+10);
+  colors = hsv(curves);
+  black = [0 0 0];
+  set(gca, 'ColorOrder', [ black; colors((1:3)+(p-1)*3,:)], 'FontSize', fontsize);
+  axis([t0 td -2 td], 'square');
+  hold all;
+end
 
-% Figure g
-g = figure;
-fontsize = 14;
-title('Nondimensionalized Progress vs Time', 'FontSize', fontsize+5);
-ylabel('\Psi^{\prime}',  'FontSize', fontsize+10, 'rot', 0);
-xlabel('\Psi', 'FontSize', fontsize+10);
-set(gca, 'ColorOrder', hsv(curves), 'FontSize', fontsize);
-axis([-2 5 -2 5], 'square');
-hold all;
-
-labels = cell(curves, 1);
-labelCount = 1;
-
-% Plot Curves
-for p = 1:length(parameters)
-  PsiDoublePrime0 = parameters(2,p);
+% plot no-hiring case on all plots
+for p = 1:4
+  PsiDoublePrime0 = 0;
   ic = [PsiDoublePrime0 PsiPrime0 Psi0];
-	ode = @(t, Psi) [ 
-		parameters(1, p); % (Psi'')' = beta
-		Psi(1); % (Psi')' = Psi''
-		Psi(2); % (Psi)' = Psi'
-	];
-	[t, Psi] = ode45(ode, tspan, ic);
-  set(0,'CurrentFigure',f)
-	plot(t, Psi(:,3), 'LineSmoothing', 'on');
-  set(0,'CurrentFigure',g)
-  plot(Psi(:,3), Psi(:,2), 'LineSmoothing', 'on');
-	labels{p} = sprintf('\\beta = %.2f, \\gamma = %.2f', ...
-    parameters(1, p),parameters(2, p));
-  labelCount = labelCount + 1;
+  ode = @(t, Psi) [ 
+    0; % (Psi'')' = beta
+    Psi(1); % (Psi')' = Psi''
+    Psi(2); % (Psi)' = Psi'
+  ];
+  [t, Psi] = ode45(ode, tspan, ic);
+
+  subplot(sp(p));
+  plot(t, Psi(:,3), 'LineSmoothing', 'on');
+end
+
+% plot hiring cases
+for p = 1:length(gammas)
+  PsiDoublePrime0 = gammas(p);
+  subplot(sp(p));
+  labels = cell(length(betas), 1);
+  for q = 1:length(betas)
+    ic = [PsiDoublePrime0 PsiPrime0 Psi0];
+    ode = @(t, Psi) [ 
+      betas(q); % (Psi'')' = beta
+      Psi(1); % (Psi')' = Psi''
+      Psi(2); % (Psi)' = Psi'
+    ];
+    [t, Psi] = ode45(ode, tspan, ic);
+
+    
+    plot(t, Psi(:,3), 'LineSmoothing', 'on');
+
+    labels{q} = sprintf('\\gamma = %.2f, \\beta = %.2f', ...
+    gammas(p),betas(q));
+  end
+  legend(labels, 'FontSize', fontsize, 'Location', ...
+  'SouthEast');
+  legend boxoff
 end
 
 % Legend
 set(0,'CurrentFigure',f)
 set(gcf,'PaperPositionMode','auto')
 set(f, 'Position', [0 0 900 900])
-legend(labels, 'FontSize', fontsize, 'Location', ...
-'SouthEast');
-legend boxoff
-
-set(0,'CurrentFigure',g)
-legend(labels, 'FontSize', fontsize, 'Location', ...
-'SouthEast');
-legend boxoff
 
 mkdir('..','images');
 saveas(f, '../images/caseII.png');
